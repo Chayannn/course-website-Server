c8840fa155e45c642f1ebda3824dba3c60531923
Chayannn <chayanp188@gmail.com>
2023-06-02 19:00:56 +0530

    created schemas for users and courses

---
+++ models/course.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter course title"],
+    minLength: [4, "Title must be atleast 4 characters"],
+    maxLength: [80, "Title can't exceed 80 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter course title"],
+    minLength: [20, "Description must be atleast 4 characters"],
+  },
+  lectures: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      video: {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    },
+  ],
+  poster: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  numOfVideos: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: Number,
+    required: true,
+  },
+  createdBy: {
+    type: Number,
+    required: [true, "Enter Instructor Name"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Course = mongoose.model("User", schema);
